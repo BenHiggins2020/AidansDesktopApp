8d6d528e6a9d3204270edbe938d6c35e327cfe7e
Ben Higgins <higginsb2020@gmail.com>
2025-06-14 09:11:21 -0400

    Added mutableHistoricalDataRow, and extension function to map it to (non-mutable) row.

--- composeApp/src/desktopMain/kotlin/com/ben/aidansdesktopapp/Repository/HistoricalData.kt
+++ composeApp/src/desktopMain/kotlin/com/ben/aidansdesktopapp/Repository/HistoricalData.kt
@@ -9,6 +9,31 @@ data class HistoricalDataRow(
     val adjClose: String,
     val volume: String,
 )
+
+data class mutableHistoricalDataRow(
+    var date: String = "",
+    var open: String = "",
+    var high: String = "",
+    var low: String = "",
+    var close: String = "",
+    var adjClose: String = "",
+    var volume: String = "",
+)
+
+//extension function
+fun mutableHistoricalDataRow.toHistoricalDataRow(): HistoricalDataRow {
+    return HistoricalDataRow(
+        date = this.date,
+        open = this.open,
+        high = this.high,
+        low = this.low,
+        close = this.close,
+        adjClose = this.adjClose,
+        volume = this.volume
+    )
+}
+
+
 data class HistoricalData(
     val symbol: String,
     val rows: List<HistoricalDataRow>,
